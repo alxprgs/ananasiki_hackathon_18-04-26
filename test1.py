1f642f0bf25e0cba6aceb8a4b99dea89b83b5105
alxprgs <90633753+alxprgs@users.noreply.github.com>
2026-04-16 12:43:46 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,8 +3,19 @@ from raspberry.arduino_service import ArduinoService
 
 
 def main():
-    with ArduinoService() as robot:
-        robot.servo.set(0)
+    with ArduinoService(retry_count=3) as robot:
+        print(f"Попытка получения данных с первого сенсора: {robot.distance_sensor.info(1)}")
+        try:
+            print(f"Данные с первого сенсора успешно получены! Растояние в ММ: {robot.distance_sensor.get(1)}")
+        except Exception as e:
+            print(f"Ошибка получения данных с первого сенсора: {e}")
+        print(f"Попытка получения данных со второго сенсора: {robot.distance_sensor.info(2)}")
+        try:
+            print(f"Данные со второго успешно получены! Растояние в ММ: {robot.distance_sensor.get(2)}")
+        except Exception as e:
+            print(f"Ошибка получения данных со второго сенсора: {e}")
+
+
 
 if __name__ == "__main__":
     main()
