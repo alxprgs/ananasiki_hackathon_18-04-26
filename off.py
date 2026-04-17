ed22b9d8069c47bf8d2be886de2b3fafafbd3790
alxprgs <90633753+alxprgs@users.noreply.github.com>
2026-04-17 14:00:56 +0300

    test filre upd

---
+++ off.py
@@ -0,0 +1,6 @@
+from raspberry.raspberry_service import RaspberryService
+
+
+rpi = RaspberryService()
+
+rpi.disable_ap()
--- on.py
+++ on.py
@@ -3,5 +3,5 @@ from raspberry.raspberry_service import RaspberryService
 
 rpi = RaspberryService()
 
-rpi.configure_ap(ssid="HighSchool777", password="Vf65gght")
+rpi.configure_ap(ssid="Ananasiki", password="123456789")
 rpi.enable_ap()
